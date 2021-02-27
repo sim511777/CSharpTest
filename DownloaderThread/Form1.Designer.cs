17c2f2e30bc8c4f4fd4ce1f6a75185406e00b789
sim511777 <sim51177@gmail.com>
2021-02-27 16:51:50 +0900

    DownloaderThread 프로젝트 추가

---
+++ DownloaderThread/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace DownloaderThread {
+    partial class Form1 {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent() {
+            this.prbPercent = new System.Windows.Forms.ProgressBar();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // prbPercent
+            // 
+            this.prbPercent.Location = new System.Drawing.Point(43, 107);
+            this.prbPercent.Name = "prbPercent";
+            this.prbPercent.Size = new System.Drawing.Size(476, 23);
+            this.prbPercent.TabIndex = 5;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(204, 30);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(142, 61);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(43, 30);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(142, 61);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPercent.Location = new System.Drawing.Point(43, 146);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(196, 23);
+            this.lblPercent.TabIndex = 6;
+            this.lblPercent.Text = "0%";
+            this.lblPercent.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.prbPercent);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar prbPercent;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label lblPercent;
+    }
+}
+
---
+++ DownloaderThread/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloaderThread {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        Thread th = null;
+        bool thCancelReq = false;
+
+        private void ThreadFunc() {
+            thCancelReq = false;
+            int i = 0;
+            while (i < 100) {
+                // 작업 Cancel 처리
+                if (thCancelReq) {
+                    this.Invoke((Action)delegate() {
+                        lblPercent.Text = $"{i}% Canceled.";
+                    });
+                    break;
+                }
+
+                Thread.Sleep(100);
+                i++;
+
+                // CrossThread UI Access
+                this.Invoke((Action)delegate() {
+                    prbPercent.Value = i;
+                    lblPercent.Text = $"{i}% Downloading...";
+                });
+            }
+        }
+
+        private void StartDownload() {
+            // 쓰레드가 null이 아니고 stop 상태가 아니라면 시작 할 수가 없다
+            if (th != null && th.ThreadState.HasFlag(ThreadState.Stopped) == false) {
+                MessageBox.Show("Error - Already Downloading...");
+                return;
+            }
+
+            th = new Thread(ThreadFunc);
+            th.Start();
+        }
+
+        private void CancelDownload() {
+            // 쓰레드가 null이거나 Stop 상태라면 Cancel 할 수가 없다
+            if (th == null || th.ThreadState.HasFlag(ThreadState.Stopped)) {
+                MessageBox.Show("Error - No Downloading...");
+                return;
+            }
+
+            thCancelReq = true;
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e) {
+            StartDownload();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            CancelDownload();
+        }
+    }
+}
