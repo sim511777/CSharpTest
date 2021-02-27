26d4446a2ba01c0d924a5121d26de3ce201fda1c
sim511777 <sim51177@gmail.com>
2021-02-27 18:14:32 +0900

    DownloaderBackgroundWorker 프로젝트 추가

--- DownloaderBackgroundWorker/Form1.cs
+++ DownloaderBackgroundWorker/Form1.cs
@@ -5,52 +5,43 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
-namespace DownloaderThread {
+namespace DownloaderBackgroundWorker {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
         }
 
-        Thread th = null;
-        bool thCancelReq = false;
-
-        private void ThreadFunc() {
-            thCancelReq = false;
+        private void bgw_DoWork(object sender, DoWorkEventArgs e) {
             int i = 0;
             while (i < 100) {
-                // Cancel 처리
-                if (thCancelReq)
+                // cancel 처리
+                if (bgw.CancellationPending) {
+                    e.Cancel = true;
                     break;
+                }
 
                 Thread.Sleep(50);
                 i++;
 
                 // 진행 보고
-                this.Invoke((Action)delegate() {
-                    ReportProgress(i);
-                });
+                bgw.ReportProgress(i);
             }
-
-            // 결과 보고
-            this.Invoke((Action)delegate () {
-                ReportComplete();
-            });
         }
 
         // 진행 보고
-        private void ReportProgress(int percent) {
-            prbPercent.Value = percent;
-            lblPercent.Text = $"{percent}%";
+        private void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+            prbPercent.Value = e.ProgressPercentage;
+            lblPercent.Text = $"{e.ProgressPercentage}%";
             lblLog.Text = "Downloading...";
         }
 
         // 결과 보고
-        private void ReportComplete() {
-            if (thCancelReq) {
+        private void bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            if (e.Cancelled) {
                 lblLog.Text = "Canceled.";
             } else {
                 lblLog.Text = "Completed.";
@@ -58,22 +49,21 @@ namespace DownloaderThread {
         }
 
         private void btnDownload_Click(object sender, EventArgs e) {
-            if (th != null && th.ThreadState.HasFlag(ThreadState.Stopped) == false) {
+            if (bgw.IsBusy) {
                 MessageBox.Show("Error - Already Downloading...");
                 return;
             }
 
-            th = new Thread(ThreadFunc);
-            th.Start();
+            bgw.RunWorkerAsync();
         }
 
         private void btnCancel_Click(object sender, EventArgs e) {
-            if (th == null || th.ThreadState.HasFlag(ThreadState.Stopped)) {
+            if (!bgw.IsBusy) {
                 MessageBox.Show("Error - No Downloading...");
                 return;
             }
 
-            thCancelReq = true;
+            bgw.CancelAsync();
         }
     }
 }
