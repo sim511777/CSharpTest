646bfe887afcb951248a08d1a638822824a54fb3
sim511777 <sim51177@gmail.com>
2021-02-27 13:24:14 +0900

    TaskTest 프로젝트 추가

---
+++ TaskTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskTest {
+    class Program {
+        public static int LongCalc() {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            for (var i = 0; i < 50; i++) {
+                Console.WriteLine($"ThreadId = {threadId}, i={i}");
+                Thread.Sleep(100);
+            }
+            return threadId;
+        }
+
+        static void Main(string[] args) {
+            int taskNum = 10;
+            
+            // 동기호출
+            //var results = new int[taskNum];
+            //for (var i = 0; i < taskNum; i++) {
+            //    results[i] = LongCalc();
+            //}
+            //for (var i = 0; i < taskNum; i++) {
+            //    Console.WriteLine($"results[{i}]={results[i]}");
+            //}
+
+            // Task로 비동기 호출
+            var tasks = new Task<int>[taskNum];
+            for (var i = 0; i < taskNum; i++) {
+                // ThreadPool에서 task 함수 () => LongCalc() 호출
+                tasks[i] = Task.Run(() => LongCalc());
+            }
+
+            for (var i = 0; i < taskNum; i++) {
+                // task 함수 완료까지 대기
+                tasks[i].Wait();
+            }
+
+            for (var i = 0; i < taskNum; i++) {
+                // taks 함수 결과 표시
+                Console.WriteLine($"tasks[{i}.Result={tasks[i].Result}");
+            }
+        }
+    }
+}
