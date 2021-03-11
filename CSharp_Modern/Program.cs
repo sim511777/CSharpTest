5b45f8ad08da8a260c8e404868a7c45c41a1d121
sim511777 <sim51177@gmail.com>
2021-03-11 18:06:07 +0900

    Switch 식

---
+++ CSharp_Modern/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+int FibOld(int i) {
+    if (i <= 2) {
+        return 1;
+    } else {
+        return FibOld(i - 2) + FibOld(i - 1);
+    }
+}
+
+int Fib(int i) =>
+    i switch {
+        int when i <= 2 => 1,
+        _ => Fib(i + 2) + Fib(i - 1),
+    };
+
+WriteLine($"20번째: {Fib(20)}");
+WriteLine($"5번째: {Fib(5)}");
