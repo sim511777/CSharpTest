82cc14360336965b498ab1b7749d303bb7583603
sim511777 <sim51177@gmail.com>
2021-06-15 23:06:42 +0900

    JustTest 프로젝트 추가

---
+++ JustTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustTest {
+    class Program {
+        static void Main(string[] args) {
+            Derived derived = new Derived();
+            derived.Fly();
+        }
+    }
+
+    class Base {
+        public void Run() => Console.WriteLine("Base.Run()");
+        public void Fly() => Console.WriteLine("Base.Fly()");
+    }
+
+    class Derived : Base {
+        public void NewFly() => Console.WriteLine("Derived.Fly()");
+        public new void Fly() => Console.WriteLine("Derived.Fly()");
+    }
+}
